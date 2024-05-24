866fae36f52b67afa91026766aa6675723bfdb99
khallaf <engayman.khalaf@gmail.com>
2024-05-24 09:04:18 +0200

    create quiz section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 //start game button
-document.getElementById("start-game_btn").addEventListener("click", () => document.getElementById("start-game").classList.toggle("hidden"));
+document.getElementById("start-game_btn").addEventListener("click", () => { document.getElementById("start-game").classList.toggle("hidden");
+document.getElementById("quiz").classList.remove("hidden")
+ });
 
