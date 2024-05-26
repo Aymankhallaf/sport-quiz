9db79113e640ea0a51e76975a1785a14590361a7
khallaf <engayman.khalaf@gmail.com>
2024-05-26 21:30:35 +0200

    aswer template done

--- js/ui.js
+++ js/ui.js
@@ -4,10 +4,32 @@ export function displayBoard(game) {
     clone.querySelector('.js-quiz-ttl').innerText = `Question ${game.nQuestion} `;
     clone.querySelector('.js-quiz-score').innerText = `Score ${game.score} `;
     clone.querySelector('.js-quiz__question').innerText = game.currentQuestion.qText;
-    //clone.querySelector('.js-quiz__question').innerText = game.currentQuestion.qText ;
-    const quizSection=document.getElementById("quiz")
+    //answers
+    const choices = shuffle(game.currentQuestion.choices);
+    const answersTemplate = document.getElementById("answers-template");
+    const answerSection = document.querySelector(".js-quiz__question__answers");
+    choices.forEach(a => {
+        console.log(a)
+        const answerClone = document.importNode(answersTemplate.content, true);
+        const answerTag = answerClone.querySelector('.js-quiz__answer');
+        answerTag.innerText = a;
+        answerSection.appendChild(answerTag);
+    });
+    const quizSection = document.getElementById("quiz");
+    quizSection.innerHTML = '';
     quizSection.appendChild(clone);
+    quizSection.appendChild(answerSection);
 
-    console.log(quizTemplate)
+    console.log(answerSection)
 
 }
+
+
+/**
+ * Shuffles any array.
+ * @param {*} array 
+ * @returns the same array after shuffling
+ */
+function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+};
